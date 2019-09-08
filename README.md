3e4f87d816c4db8ea137b1ddf39d4758cc339221
CruzAzul <no-reply>
2019-09-08 19:40:24 -0400

    buy and update stk functions

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # Bamazon
 
+This is the node app called **Bamazon** to buy products and keep, in the **SQL Database**, the stock up-to-date at all times.
+
+First, we display, by calling the **SQL Bamazon Database**, the entire stock of items with names and descriptions, see below:
+
 ![](displayAll.png)
+
+Secondly, we capture, through **Inquirer Prompts**, the inputs from the user, see below:
+
+![](Inquirer_Prompts.png)
+
+As a third step, we connect to the **SQL Bamazon DB** again, and, based on the purchases from the users, we maintain the stock on hand up-to-date. 
+In this case, I got a challenge connecting to the DB, find my code in the repo [My repo in Github!](https://github.com/fpeniche/Bamazon). 
+
+For some reason, the app stops once I capture the prompts from the user, as seen in image above, and didn't connect to the DB as the next step. See the functions with SQL scripts I plan to run:
+
+Function to buy the product
+![](Buy_Product.png)
+
+Function to update stock
+![](Update_Stock.png)
+
+And, my DB connection
+![](MyDB_Connection.png)
+
 
--- bamazon.js
+++ bamazon.js
@@ -1,13 +1,13 @@
 // dependencies for inquirer and mysql npm packages
 var inquirer = require("inquirer");
-function Product(prodId, stock) {
-    this.prodId = prodId;
-    this.stock = stock;
+/* function Product(productId, units) {
+    this.productId = productId;
+    this.units = units;
     // creates the printInfo method and applies it to all Product objects
     this.printInfo = function() {
-      console.log("Product ID: " + this.prodId + "\nStock: " + this.stock);
+      console.log("Product ID: " + this.productId + "\nStock: " + this.units);
     };
-  }
+  } */
 var mysql = require("mysql");
 var connection=mysql.createConnection({
     host:"localhost",
@@ -17,56 +17,11 @@ var connection=mysql.createConnection({
     database:"BAMAZON"
 });
 
-function buyProduct()
-    {
-           inquirer.prompt([
-                    {
-                    name: "productId",
-                    message: "What is the product ID you want to buy?"
-                    }, {
-                    name: "units",
-                    message: "How many units of the product you want to buy?"
-                    }
-                ]).then(function(answers) {
-                    var newProductId = new Product(answers.productId);
-                    var newUnits = new Product(answers.units);
-                  //  var productId = (answers.productId);
-                  //  var units = (answers.units);
-                    newProductId.printInfo();
-                    newUnits.printInfo();
-
-                    }); 
-                    
-                connection.query("SELECT ITEM_ID, STOCK_QTY FROM PRODUCTS", function(error,res){
-                if(error)
-                {
-                    console.log(error);
-                }
-                else
-                {
-                    var dbItemId = res[i].ITEM_ID;
-                    var dbStockQty = res[i].STOCK_QTY;
-
-                    if(dbItemId==productId)
-                    {
-                        if(dbStockQty - units>=0)
-                        {
-                            console.log("Your order has been placed!");
-                            updateStock(dbItemId,dbStockQty);
-                        }
-                        else
-                        {
-                            console.log("Not enough stock on hand, your item will be backordered");
-                        }
-                    }
-                }
-            }) 
-           
-          }
+// functions created
 
 function updateStock(dbItemId,dbStockQty)
-          {
-            var stkOnHand = dbStockQty - units;
+        {
+            var stkOnHand = dbStockQty - newUnits;
               //here is where we can write our query to connec to DB
               //the placeholder(?) is used to avoid SQL injection and take the name value in the user input as value
               connection.query("UPDATE PRODUCTS SET STOCK_QTY=? WHERE ITEM_ID=?",[stkOnHand,dbItemId], function(error,res){
@@ -79,7 +34,77 @@ function updateStock(dbItemId,dbStockQty)
                         console.log("Item " +dbItemId+ "stock on hand is now = " +stkOnHand);
                   }
               })
-          }
+        }
+
+function buyProduct(newProductId,newUnits)
+    {
+            connection.query("SELECT ITEM_ID, STOCK_QTY FROM PRODUCTS WHERE ITEM_ID=?",[newProductId], function(error,res){
+                if(error)
+                {
+                    console.log(error);
+                }
+                else
+                {
+                    var dbItemId = res.ITEM_ID;
+                    var dbStockQty = res.STOCK_QTY;
+
+                    if(dbItemId==newProductId)
+                    {
+                        if(dbStockQty - newUnits>=0)
+                        {
+                            console.log("Your order has been placed!");
+                            updateStock(dbItemId,dbStockQty);
+                        }
+                        else
+                        {
+                            console.log("Not enough stock on hand, your item will be backordered");
+                        }
+                    }
+                }
+            }) 
+    }
+
+function captureInquirerData()
+{
+    inquirer.prompt([
+                    {
+                    name: "productId",
+                    message: "What is the product ID you want to buy?"
+                    }, {
+                    name: "units",
+                    message: "How many units of the product you want to buy?"
+                    }
+                ]).then(function(answers) {
+                    var newProductId = answers.productId;
+                    var newUnits = answers.units;
+                    console.log("You've ordered: \nProduct ID: " + newProductId + "\nUnits: " + newUnits);
+                  //  var productId = (answers.productId);
+                  //  var units = (answers.units);
+                  //  newProductId.printInfo();
+                  //  newUnits.printInfo();
+                  //  return newProductId, newUnits;
+                   connection.connect(function(error){
+                    if(error)
+                    {
+                        console.log("Connection not working due to: " +error);
+                    }
+                    else
+                    {
+                        //here is where we can write our query to connect to DB
+                      //  readBamazon(); 
+
+                        buyProduct(1000,5);
+                        
+                    }
+                })
+              /*  timesToRun--;
+                if(timesToRun>0)
+                {
+                    captureInquirerData(timesToRun);
+                } */
+
+                });
+}
 
 function readBamazon()
         {
@@ -104,20 +129,10 @@ function readBamazon()
         }) 
         }
 
+// functions executed
+        captureInquirerData();
 
-connection.connect(function(error){
-    if(error)
-    {
-        console.log(error);
-    }
-    else
-    {
-        //here is where we can write our query to connec to DB
-      //  readBamazon(); 
-        buyProduct();
 
-    }
-})
 
 
 
